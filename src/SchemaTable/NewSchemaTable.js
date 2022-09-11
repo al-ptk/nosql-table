b1ddeda155fa2f487cf88085d7db3ed4e2cda7cd
Alan Patrick <alan.patrick.fv@gmail.com>
2022-09-11 10:56:45 -0300

    Clean up SchemaTable directory

--- src/SchemaTable/NewSchemaTable.js
+++ src/SchemaTable/NewSchemaTable.js
@@ -1,50 +1,6 @@
 import React from 'react';
-import HeadingRow from './HeadingRow';
-import TableData, { AddRowButton } from './TableData';
 import { StyledTable } from './SchemaTable.styled';
 
-export function SchemaTable({ headings, setHeadings, rows, setRows }) {
-  function updateHeading(event, index) {
-    // Update headings list
-    const newHeadings = [...headings];
-    newHeadings[index] = event.target.value;
-    setHeadings(newHeadings);
-  }
-
-  function updateCell(event, cellIndex, rowIndex) {
-    const newRows = [...rows];
-    newRows[rowIndex][cellIndex] = event.target.value;
-    setRows(newRows);
-  }
-
-  function addColumn() {
-    setHeadings(headings.concat(`property ${headings.length}`));
-  }
-
-  function addRow() {
-    const newRows = [...rows];
-    newRows.push({});
-    setRows(newRows);
-  }
-
-  return (
-    <StyledTable>
-      <tbody>
-        <HeadingRow
-          headings={headings}
-          updateHeading={updateHeading}
-          addColumn={addColumn}
-        />
-
-        <TableData
-          rows={rows}
-          headings={headings}
-          updateCell={updateCell}
-          addRow={addRow}
-        />
-
-        <AddRowButton addRow={addRow} />
-      </tbody>
-    </StyledTable>
-  );
+export function SchemaTable({ table, setTable }) {
+  return <StyledTable>Hey</StyledTable>;
 }
