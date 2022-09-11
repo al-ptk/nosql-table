e75bf21b3580a346e4d80f870e5d896d9020ed47
Alan Patrick <alan.patrick.fv@gmail.com>
2022-09-11 10:21:32 -0300

    Clean up code for removing warnings

--- src/SchemaTable/NewSchemaTable.js
+++ src/SchemaTable/NewSchemaTable.js
@@ -2,34 +2,13 @@ import React from 'react';
 import HeadingRow from './HeadingRow';
 import TableData, { AddRowButton } from './TableData';
 import { StyledTable } from './SchemaTable.styled';
-import { useEffect } from 'react';
 
 export function SchemaTable({ headings, setHeadings, rows, setRows }) {
-  useEffect(() => {
-    if (!headings.length && rows.length === 1) {
-      addColumn();
-    }
-  }, []);
-
   function updateHeading(event, index) {
-    /**
-     * newValue refers to the new column heading
-     * oldValue refers to the column heading being replaced
-     */
-    const newValue = event.target.value;
-    const oldValue = headings[index];
-    
     // Update headings list
     const newHeadings = [...headings];
-    newHeadings[index] = newValue;
+    newHeadings[index] = event.target.value;
     setHeadings(newHeadings);
-
-    // Update all objects on the rows list
-    const newRows = [...rows];
-    newRows.forEach((row) => {
-      const {newValue: oldValue, ...newObj} = row;
-      row = {newValue, ...newObj}
-    });
   }
 
   function updateCell(event, cellIndex, rowIndex) {
