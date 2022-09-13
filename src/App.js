24fe168bd72fd1b3013d07efa392de8a540db3e1
Alan Patrick <alan.patrick.fv@gmail.com>
2022-09-13 16:33:23 -0300

    Try something new and fail

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import SchemaTable from './SchemaTable';
 import { GlobalStyle } from './styled-globals';
-import { useState } from 'react';
-import currentTable from './mockTable.json';
+// import { useState } from 'react';
+// import currentTable from './mockTable.json';
+
+const properties = ['Prop 1', 'Prop 2'];
+const dataset = [
+  ['Value 1', 'Value 2'],
+  ['Value 3', 'Value 4'],
+];
 
 function App() {
-  const [table, setTable] = useState([...currentTable]);
+  // const [table, setTable] = useState([...currentTable]);
 
   return (
     <>
       <GlobalStyle />
-      <SchemaTable {...{ table, setTable }} />
+      <SchemaTable dataset={dataset} properties={properties}/>
     </>
   );
 }
--- src/SchemaTable/NewSchemaTable.js
+++ src/SchemaTable/NewSchemaTable.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyledTable } from './SchemaTable.styled';
 
-export function SchemaTable({ table, setTable }) {
+export function SchemaTable(props) {
   return (
     <StyledTable>
       <tbody>
