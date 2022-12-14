8648f8215585b2f93dd307aaddd4e6053ec95a07
Alan Patrick <alan.patrick.fv@gmail.com>
2022-12-14 10:45:19 -0300

    fix: Set cursor state to be object

--- src/hooks/useCursor.js
+++ src/hooks/useCursor.js
@@ -1,41 +1,8 @@
 export default function useCursor() {
   const cursorState = {
     position: [null, null],
-    mode: 'normal',
-  };
-
-  const moveCursor = (direction) => {
-    switch (direction) {
-      case 'ArrowUp':
-        console.log('up');
-        break;
-      case 'ArrowRight':
-        console.log('right');
-        break;
-      case 'ArrowDown':
-        console.log('down');
-        break;
-      case 'ArrowLeft':
-        console.log('left');
-        break;
-      default:
-        break;
-    }
-  };
-
-  const handleArrowKeys = (e) => {
-    if (cursorState.mode !== 'normal') return;
-
-    e.preventDefault();
-    const keyPressed = e.nativeEvent.key;
-    const acceptedKeys = ['ArrowUp', 'ArrowRight', 'ArrowDown', 'ArrowLeft'];
-    if (acceptedKeys.includes(keyPressed)) {
-      moveCursor(keyPressed);
-    }
-    return;
   };
 
   return {
-    handleArrowKeys,
   };
 }
