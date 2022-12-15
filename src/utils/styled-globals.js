1908a9ce3b6bf86b389f4f69905b63906b8c6d43
Alan Patrick <alan.patrick.fv@gmail.com>
2022-12-15 10:48:46 -0300

    style: Body

--- src/utils/styled-globals.js
+++ src/utils/styled-globals.js
@@ -3,12 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   body {
     min-height: 100vh;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-
-    background-color: hsl(195, 45%, 75%);
+    background-color: #252525;
   }
 
   textarea {
