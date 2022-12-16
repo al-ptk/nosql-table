2aa127f875c333d229af20ea000178cb4a73aa7b
Alan Patrick <alan.patrick.fv@gmail.com>
2022-12-16 17:27:58 -0300

    fix: Purge late context stuff from App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import JsonTable from './JsonTable';
 import { GlobalStyle } from './utils/styled-globals';
-import { useState } from 'react';
 import { ActionBar } from './ActionBar/ActionBar';
 import { StyledFooter } from './StyledFooter';
-import useTableManager from './hooks/useTableManager';
 import { Provider as StoreProvider } from 'react-redux';
 import store from './app/store';
-import { createContext } from 'react';
-export const AppStateContext = createContext();
 
 function App() {
-  const tableManager = useTableManager();
-  const [showPreview, setShowPreview] = useState(false);
-
-  const AppState = {
-    ...tableManager,
-    showPreview,
-    setShowPreview,
-  };
-
   return (
     <>
       <GlobalStyle />
