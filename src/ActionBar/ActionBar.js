dbe34a8891dd6678f323bef90d52594902fde74b
Alan Patrick <alan.patrick.fv@gmail.com>
2022-12-17 11:03:42 -0300

    fix: ActionBar uses redux store api

--- src/ActionBar/ActionBar.js
+++ src/ActionBar/ActionBar.js
@@ -1,19 +1,19 @@
-import { useContext } from 'react';
-import { AppStateContext } from '../App';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  updateTitle,
+  newTable,
+  addInstance,
+  addProperty,
+} from '../app/slices/tableSlice';
 import { StyledActionBar } from './ActioBar.styled';
 import { ExportDataButton } from './ExportDataButton';
 import { ImportDataButton } from './ImportDataButton';
 
 export function ActionBar() {
-  const {
-    addRow,
-    addColumn,
-    title,
-    setTitle,
-    showPreview,
-    setShowPreview,
-    newTable,
-  } = useContext(AppStateContext);
+  const [showPreview, setShowPreview] = useState(false);
+  const title = useSelector((state) => state.table.title);
+  const dispatch = useDispatch();
 
   const togglePreview = (e) => {
     if (!showPreview) {
@@ -27,17 +27,17 @@ export function ActionBar() {
 
   return (
     <StyledActionBar>
-      <button onClick={() => newTable()}>New Table</button>
+      <button onClick={() => dispatch(newTable())}>New Table</button>
       <ImportDataButton />
       <ExportDataButton />
       <input
         type={'text'}
         value={title}
-        onInput={(e) => setTitle(e.target.value)}
+        onInput={(e) => dispatch(updateTitle({ newTitle: e.target.value }))}
         className="title"
       />
-      <button onClick={() => addColumn()}>Add Column</button>
-      <button onClick={() => addRow()}>Add Row</button>
+      <button onClick={() => dispatch(addProperty())}>Add New Property</button>
+      <button onClick={() => dispatch(addInstance())}>Add New Instance</button>
       <button onClick={togglePreview}>Show Preview</button>
     </StyledActionBar>
   );
