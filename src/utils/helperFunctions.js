8307cac40270bfecfd788ada030a56c1a0f10bfe
Alan Patrick <alan.patrick.fv@gmail.com>
2022-12-20 12:18:58 -0300

    fix: Clean up helperFunctions

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -5,34 +5,10 @@
  */
 export function getAllKeys(objList) {
   let result = [];
-
   for (const obj of objList) {
-    // get the keys list of current obj
     const keys = Object.keys(obj);
-
-    // Make array of unregistered keys
     const notInResult = keys.filter((key) => !result.includes(key));
-
-    // add not-repeated keys to result array
     result.push(...notInResult);
   }
-
   return result;
-}
-
-/**
- * @param {Int} length
- * @param {Int} start
- * @param {Int} step
- * @returns An array of integers
- */
-export function range(length, start = 0, step = 1) {
-  return Object.keys(Array(length).fill(null));
-}
-
-export function* counterGenerator(start = 0) {
-  while (true) {
-    yield start;
-    start++;
-  }
 }
