19c4effe6cb536b61415e6a2cdc37562e9e698b4
Alan Patrick <alan.patrick.fv@gmail.com>
2022-12-28 15:24:36 -0300

    style: Improve JSONPreview

--- src/modals/JSONPreview/JSONPreview.js
+++ src/modals/JSONPreview/JSONPreview.js
@@ -3,10 +3,10 @@ import { createPortal } from 'react-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setModal } from '../../redux/slices/uiKnobsSlice';
 import { Modal } from '../Modal.styles';
-import { StyledJSONPreview } from './JSONPreview.styles';
+import JsonFormatter from 'react-json-formatter';
+import styled from 'styled-components';
 
 export function JSONPreview() {
-  const instances = useSelector((state) => state.table.instances);
   const dispatch = useDispatch();
 
   return createPortal(
@@ -15,12 +15,38 @@ export function JSONPreview() {
         <Modal.CloseButton onClick={() => dispatch(setModal({}))}>
           <Modal.CloseIcon />
         </Modal.CloseButton>
-        <StyledJSONPreview.Formatter
-          json={JSON.stringify(instances)}
-          tabWith={4}
-        />
+        <StyledFormatter />
       </Modal.Container>
     </Modal.Backdrop>,
     document.querySelector('#modal-portal')
   );
 }
+
+const StyledFormatter = () => {
+  const instances = useSelector((state) => state.table.instances);
+  return (
+    <Div
+      style={{
+        backgroundColor: '#303030',
+        padding: 20,
+        width: '100%',
+        height: '400px',
+        overflowY: 'scroll',
+      }}
+    >
+      <JsonFormatter
+        json={JSON.stringify(instances)}
+        tabWith={4}
+        style={{ backgroundColor: '#303030' }}
+      />
+    </Div>
+  );
+};
+
+const Div = styled.div`
+  background-color: #303030;
+  padding: 20px;
+  width: 100%;
+  height: 400px;
+  overflow-y: scroll;
+`;
