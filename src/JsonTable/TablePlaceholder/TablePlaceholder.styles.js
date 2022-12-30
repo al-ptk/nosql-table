3c7e232c5f0c43dfe44495f0e888139b03af6032
Alan Patrick <alan.patrick.fv@gmail.com>
2022-12-30 15:19:01 -0300

    style: mobile-proof tableplaceholder

--- src/JsonTable/TablePlaceholder/TablePlaceholder.styles.js
+++ src/JsonTable/TablePlaceholder/TablePlaceholder.styles.js
@@ -2,29 +2,64 @@ import styled from 'styled-components';
 
 export const StyledTP = {
   Container: styled.div`
+    /* External Layout */
+    position: relative;
+    margin: 100px auto;
+    width: 70vw;
+    max-width: 900px;
+    height: 40vh;
+
+    /* Internal Layout */
     display: flex;
     justify-content: center;
     align-items: center;
     gap: 10%;
-    position: relative;
-    margin: 100px auto;
-    width: 60vw;
-    max-width: 560px;
-    height: 40vh;
+    padding: 20px 0;
+
+    /* Modifiers */
     background-color: transparent;
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.7);
     border-radius: 15px;
+
+    @media screen and (max-width: 800px) {
+      /* External Layout */
+      width: 90vw;
+
+      /* Internal Layout */
+      gap: 5%;
+      padding: 0;
+
+      /* Modifiers */
+      /* @dryup @fontSizes */
+      font-size: 10px !important;
+    }
   `,
   Button: styled.button`
+    /* External layout */
+    height: fit-content;
+    font-size: 24px;
+    min-width: 100px;
+
+    /* Internal layout */
+    padding: 0px 20px;
+
+    /* Modifiers */
+    border-radius: 15px;
     color: white;
     border: 1px solid;
     background-color: transparent;
     font-weight: bold;
-    height: 2em;
-    font-size: 24px;
-    min-width: 100px;
-    padding: 0px 20px;
-    border-radius: 15px;
+
+    @media screen and (max-width: 800px) {
+      /* External Layout */
+      min-width: 100px;
+
+      /* Internal layout */
+      /* @dryup @fontSizes */
+      font-size: 20px;
+
+      /* Modifiers */
+    }
   `,
   Ruler: styled.hr`
     height: 80%;
