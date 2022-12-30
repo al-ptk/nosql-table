69a738d7f8e156d4dcd91fadd34495b117b8a8c8
Alan Patrick <alan.patrick.fv@gmail.com>
2022-12-30 15:58:03 -0300

    mess: Jumble some stuff up

--- src/components/DropDown.styles.js
+++ src/components/DropDown.styles.js
@@ -1,43 +1,55 @@
 import styled from 'styled-components';
 
-export const DropDownAnchor = {
-  Container: styled.div``,
-  Button: styled.button`
-    padding: 5px;
-    background-color: transparent;
-    color: white;
-    border: none;
-    font-weight: normal;
-    font-size: 19px;
-  `,
-};
-
 export const DropDown = {
   Container: styled.div`
+    /* External layout */
     position: fixed;
     top: ${(props) => props.yPos};
     left: ${(props) => props.xPos};
     z-index: 10;
     width: fit-content;
+
+    /* Internal layout */
+    padding: 5px 0px;
+
+    /* Modifiers */
     background-color: #373737;
     border: 1px solid white;
-    padding: 5px 0px;
   `,
   Button: styled.button`
+    /* External layout */
     display: block;
-    background-color: transparent;
-    color: white;
-    border: none;
     width: 100%;
+
+    /* Internal layout */
     text-align: left;
     padding: 1px 10px;
     font-size: 16px;
 
+    /* Modifiers */
+    background-color: transparent;
+    color: white;
+    border: none;
     :disabled {
       color: rgba(255, 255, 255, 0.3);
     }
   `,
   HorRuler: styled.hr`
     margin: 5px 0;
+  `,
+};
+
+export const DropDownAnchor = {
+  Container: styled.div``,
+  Button: styled.button`
+    /* Internal layout */
+    padding: 5px;
+    font-size: 19px;
+
+    /* Modifiers */
+    background-color: transparent;
+    color: white;
+    border: none;
+    font-weight: normal;
   `,
 };
