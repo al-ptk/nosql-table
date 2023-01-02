a4b8089dbfa0a612174b1748da3e1f5bf522d2f7
Alan Patrick <alan.patrick.fv@gmail.com>
2023-01-02 13:56:03 -0300

    feat: make ExpandMobileMenuButton render acccording to breakpoint

--- src/MenuBar/TitleInput/TitleInput.js
+++ src/MenuBar/TitleInput/TitleInput.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateTitle } from '../../redux/slices/tableSlice';
@@ -6,27 +7,36 @@ import { StyledTitleInput } from './TitleInput.styles';
 export const TitleInput = () => {
   const dispatch = useDispatch();
   const title = useSelector((state) => state.table.title);
-  const [mobileExpanded, setMobileExpanded] = useState(true); // it has to start at true for the toggling to work
-  // @todo fix this toggling behavior to make more sense
+  const [mobileExpanded, setMobileExpanded] = useState(false);
+
+  useEffect(() => {
+    // Expand or contract mobile menu based on state
+    const newHeight = mobileExpanded ? 'fit-content' : '3em';
+    document.querySelector('#MenuBar').style.height = newHeight;
+  }, [mobileExpanded]);
 
   return (
-    <div>
-      <StyledTitleInput
+    <StyledTitleInput.Container>
+      <StyledTitleInput.Input
         type={'text'}
         value={title}
         onInput={(e) => dispatch(updateTitle({ newTitle: e.target.value }))}
         className="title"
       />
-      <button
-        id="toggle-mobile-menu"
-        onClick={() => {
-          setMobileExpanded((b) => !b);
-          const newHeight = mobileExpanded ? 'fit-content' : '3em';
-          document.querySelector('#MenuBar').style.height = newHeight;
-        }}
-      >
-        v
-      </button>
-    </div>
+      <ExpandMobileMenuButton {...{ mobileExpanded, setMobileExpanded }} />
+    </StyledTitleInput.Container>
+  );
+};
+
+const ExpandMobileMenuButton = ({ mobileExpanded, setMobileExpanded }) => {
+  return (
+    <StyledTitleInput.MobileMenuToggler
+      id="toggle-mobile-menu"
+      onClick={() => {
+        setMobileExpanded((b) => !b);
+      }}
+    >
+      {mobileExpanded ? '^' : 'v'}
+    </StyledTitleInput.MobileMenuToggler>
   );
 };
--- src/MenuBar/TitleInput/TitleInput.styles.js
+++ src/MenuBar/TitleInput/TitleInput.styles.js
@@ -1,19 +1,52 @@
 import styled from 'styled-components';
 
-export const StyledTitleInput = styled.input`
-  /* External layout */
-  width: 200px;
-  margin: 5px 0px;
-  padding: 0 7px;
-  order: 0;
+export const StyledTitleInput = {
+  Container: styled.div`
+    /* Internal layout */
+    display: flex;
+    align-items: baseline;
 
-  /* Internal layout */
-  font-size: 18px;
+    @media screen and (max-width: 600px) {
+      margin-top: 5px;
+    }
+  `,
+  Input: styled.input`
+    /* External layout */
+    width: 200px;
+    margin: 5px 0px;
+    padding: 0 7px;
+    order: 0;
 
-  /* Modifiers */
-  background-color: transparent;
-  color: white;
-  outline: none;
-  border: none;
-  border-bottom: 1px solid;
-`;
+    /* Internal layout */
+    font-size: 18px;
+
+    /* Modifiers */
+    background-color: transparent;
+    color: white;
+    outline: none;
+    border: none;
+    border-bottom: 1px solid;
+  `,
+  MobileMenuToggler: styled.button`
+    /* External layout */
+    width: 24px;
+    height: 24px;
+    margin-left: 15px;
+
+    /* Internal layout */
+    display: grid;
+    place-items: center;
+    padding: 0 0 2px 2px;
+
+    /* Modifiers */
+    border: none;
+    background-color: rgba(137, 137, 137, 0.5);
+    color: white;
+    border-radius: 50%;
+    font-weight: bold;
+
+    @media screen and (min-width: 600px) {
+      display: none;
+    }
+  `,
+};
